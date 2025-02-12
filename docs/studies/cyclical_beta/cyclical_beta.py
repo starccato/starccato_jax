050399b1914de36b0e0964b7518f47c600c5058e
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-12 13:27:26 +1300

    add monotonic schedule investigation

--- docs/studies/cyclical_beta/cyclical_beta.py
+++ docs/studies/cyclical_beta/cyclical_beta.py
@@ -1,10 +1,11 @@
 import os
 
 from starccato_jax.data import load_data
-from starccato_jax.trainer import Config, train_vae
 from starccato_jax.sampler import sample_latent_vars_given_data
+from starccato_jax.trainer import Config, train_vae
 
 HERE = os.path.dirname(__file__)
+OUT_BETA_RAMP = os.path.join(HERE, "out_models/beta_monotonic")
 OUT_BETA_1 = os.path.join(HERE, "out_models/beta_1")
 OUT_BETA_0 = os.path.join(HERE, "out_models/beta_0")
 OUT_BETA_CYCLICAL = os.path.join(HERE, "out_models/beta_cyclical")
@@ -15,12 +16,26 @@ config_args = dict(latent_dim=Z_SIZE, epochs=1000)
 
 kwargs = [
     dict(
+        save_dir=OUT_BETA_RAMP,
+        config=Config(
+            beta_start=0,
+            beta_end=1,
+            cyclical_annealing_cycles=1,
+            **config_args,
+        ),
+    ),
+    dict(
         save_dir=OUT_BETA_0,
         config=Config(beta_start=0, beta_end=0, **config_args),
     ),
     dict(
         save_dir=OUT_BETA_CYCLICAL,
-        config=Config(beta_start=0, beta_end=1, cyclical_annealing_cycles=3, **config_args),
+        config=Config(
+            beta_start=0,
+            beta_end=1,
+            cyclical_annealing_cycles=3,
+            **config_args,
+        ),
     ),
     dict(
         save_dir=OUT_BETA_1,
