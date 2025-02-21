791c61fbc23b46ce702e64450408c264204305b3
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-21 17:15:10 +1300

    add more rus

--- docs/studies/coverage/slurm.sh
+++ docs/studies/coverage/slurm.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=vae_mcmc
 #SBATCH --output=logs/vae_mcmc_%a.log
 #SBATCH --ntasks=1
-#SBATCH --time=00:10:00
-#SBATCH --mem=1G
+#SBATCH --time=00:03:30
+#SBATCH --mem=750MB
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-1
+#SBATCH --array=0-200
 
 ml gcc/12.3.0 python/3.11.3
 source /fred/oz303/avajpeyi/venvs/starccato_jax_venv/bin/activate
