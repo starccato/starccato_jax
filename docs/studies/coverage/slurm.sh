878c9479431b6871d72b7f52ebaa305868471ba2
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2025-02-21 17:08:25 +1300

    simple test

--- docs/studies/coverage/slurm.sh
+++ docs/studies/coverage/slurm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --job-name=vae_mcmc
-#SBATCH --output=logs/download_%a.log
+#SBATCH --output=logs/vae_mcmc_%a.log
 #SBATCH --ntasks=1
 #SBATCH --time=00:10:00
 #SBATCH --mem=1G
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-100
+#SBATCH --array=0-1
 
 ml gcc/12.3.0 python/3.11.3
 source /fred/oz303/avajpeyi/venvs/starccato_jax_venv/bin/activate
-srun python run_mcmc.py $SLURM_ARRAY_TASK_ID
+python run_mcmc.py $SLURM_ARRAY_TASK_ID
