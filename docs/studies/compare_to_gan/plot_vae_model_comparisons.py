82570d3909d5fe4cec4c564011531d5d99b0a21e
Matt Edwards <medw076@sc459141.its.auckland.ac.nz>
2025-03-28 13:37:37 +1300

    Modifying VAE arch

--- docs/studies/compare_to_gan/plot_vae_model_comparisons.py
+++ docs/studies/compare_to_gan/plot_vae_model_comparisons.py
@@ -14,6 +14,11 @@ import numpy as np
 
 from starccato_jax import Config, StarccatoVAE
 from starccato_jax.plotting.plot_distributions import plot_distributions
+from starccato_jax.plotting.plot_reconstructions import plot_reconstructions
+
+import os 
+
+HERE = os.path.dirname(__file__)
 
 
 @dataclass
@@ -24,7 +29,7 @@ class CachedSignals:
 
     @classmethod
     def load(cls):
-        with h5py.File("signal_comparisons.h5", "r") as f:
+        with h5py.File(f"{HERE}/signal_comparisons.h5", "r") as f:
             richers_signals = f["richers_signals"][:]
             gan_signals = f["gan_signals"][:]
             vae_signals = f["vae_signals"][:]
@@ -40,43 +45,66 @@ def make_plots(cache: CachedSignals, new_signals: np.ndarray):
         cache.richers_signals,
         new_signals,
         labels=["Richers", "New"],
-        fname="plts/richers_vs_new.png",
+        fname=f"{HERE}/plts/richers_vs_new.png",
         title="Richers vs New",
     )
     plot_distributions(
         cache.vae_signals,
         new_signals,
         labels=["VAE", "New"],
-        fname="plts/vae_vs_new.png",
+        fname=f"{HERE}/plts/vae_vs_new.png",
         title="VAE vs New",
     )
     plot_distributions(
         cache.gan_signals,
         new_signals,
         labels=["GAN", "New"],
-        fname="plts/gan_vs_new.png",
+        fname=f"{HERE}/plts/gan_vs_new.png",
         title="GAN vs New",
     )
+    
+    ## delete later?
     plot_distributions(
         cache.richers_signals,
         cache.gan_signals,
         labels=["Richers", "GAN"],
-        fname="plts/richers_vs_gan.png",
+        fname=f"{HERE}/plts/richers_vs_gan.png",
         title="Richers vs GAN",
     )
     plot_distributions(
         cache.richers_signals,
         cache.vae_signals,
-        labels=["GAN", "VAE"],
-        fname="plts/gan_vs_vae.png",
-        title="GAN vs VAE",
+        labels=["Richers", "VAE"],
+        fname=f"{HERE}/plts/richers_vs_vae.png",
+        title="Richers vs VAE",
     )
+    
+#    plot_reconstructions(
+#        model_data = ModelData,
+#        val_data = np.ndarray,
+#        nrows = 3,
+#        rng: jax.random.PRNGKey = None
+#    )
+
+#n = len(val_data)
+#zs = jax.random.normal(jax.random.PRNGKey(0), (n, starccato_vae.latent_dim))
+
+#generated_signal = starccato_vae.generate(z=zs)
+#for i in range(n):
+#    kwgs = dict(lw=0.1, alpha=0.1)
+#    plt.plot(generated_signal[i], color="tab:orange", **kwgs)
+#    plt.plot(val_data[i], color="k", **kwgs)
+
 
 
 def main():
-    config = Config(latent_dim=8, epochs=10, cyclical_annealing_cycles=0)
+    config = Config(latent_dim=64, 
+                    epochs=5000, 
+                    cyclical_annealing_cycles=0,
+                    learning_rate=1e-3,
+                    batch_size=32)
     vae = StarccatoVAE.train(
-        model_dir="model_out", config=config, track_gradients=True
+        model_dir=f"{HERE}/model_out", config=config, track_gradients=True
     )
     cached_sigals = CachedSignals.load()
     vae_signals = vae.generate(n=cached_sigals.n)
@@ -85,3 +113,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# Notes:
+# Need to use training set (not full Richers) to compare
+# Lowered batch size
+# Batchnorm
+
+
+# Try convolutional layers
+# Try recurrent layers (GRU)
+# Try transformer
