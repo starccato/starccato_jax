e8df22a1c3bb4843d154ddebec283fc6185a8029
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2026-07-18 15:13:07 +1200

    feat(vae): publish normalized default weights

--- src/starccato_jax/data/urls.py
+++ src/starccato_jax/data/urls.py
@@ -6,5 +6,8 @@ CCSNE_PARAMETERS_URL = f"{_ROOT_URL}/training/richers_1764_parameters.csv"
 BLIP_SIGNALS_URL = f"{_ROOT_URL}/training/gwspy_glitches.dat.zip"
 ALIGNED_DATA_URL = f"{_ROOT_URL}/training/aligned_data.h5"
 
-BLIP_WEIGHTS_URL = f"{_ROOT_URL}/weights/starcatto_jax/blip_vae.h5"
-CCSNE_WEIGHTS_URL = f"{_ROOT_URL}/weights/starcatto_jax/ccsne_vae.h5"
+_WEIGHTS_VERSION = "v0.3.0"
+_WEIGHTS_ROOT = f"{_ROOT_URL}/weights/starcatto_jax/{_WEIGHTS_VERSION}"
+
+BLIP_WEIGHTS_URL = f"{_WEIGHTS_ROOT}/blip_vae.h5"
+CCSNE_WEIGHTS_URL = f"{_WEIGHTS_ROOT}/ccsne_vae.h5"
